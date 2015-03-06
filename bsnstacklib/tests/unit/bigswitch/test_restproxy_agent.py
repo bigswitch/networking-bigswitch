6a64006a8f6b1353b99141b383e2f3625d49a105
Kevin Benton <blak111@gmail.com>
2015-03-05 21:11:24 -0800

    Fix IVS list ports method
    Fixes the IVS list ports method. There were two issues. First,
    it was sending to stderr instead of stdout like the utils.execute
    method expects by default. Second, it was calling 'list-ports',
    which did not exist on IVS.
    
    A new list-ports call is being added to IVS, but this adds fallback
    code to handle parsing 'show' output as well.
    
    Change-Id: Iae5c60b35c96e8a8341aff668b258889955bd27e

--- bsnstacklib/tests/unit/bigswitch/test_restproxy_agent.py
+++ bsnstacklib/tests/unit/bigswitch/test_restproxy_agent.py
@@ -27,6 +27,7 @@ CONSUMERCREATE = 'neutron.agent.rpc.create_consumers'
 SGRPC = 'neutron.agent.securitygroups_rpc'
 SGAGENT = 'neutron.agent.securitygroups_rpc.SecurityGroupAgentRpc'
 AGENTMOD = 'bsnstacklib.plugins.bigswitch.agent.restproxy_agent'
+IVSBRIDGE = AGENTMOD + '.IVSBridge'
 NEUTRONCFG = 'neutron.common.config'
 PLCONFIG = 'bsnstacklib.plugins.bigswitch.config'
 
@@ -42,7 +43,8 @@ class TestRestProxyAgentOVS(BaseAgentTestCase):
     def setUp(self):
         super(TestRestProxyAgentOVS, self).setUp()
         self.plapi = mock.patch(PLUGINAPI).start()
-        self.ovsbridge = mock.patch(OVSBRIDGE).start()
+        self.ovsbridge_p = mock.patch(OVSBRIDGE)
+        self.ovsbridge = self.ovsbridge_p.start()
         self.context = mock.patch(CONTEXT).start()
         self.rpc = mock.patch(CONSUMERCREATE).start()
         self.sg_agent = mock.patch(SGAGENT).start()
@@ -58,41 +60,43 @@ class TestRestProxyAgentOVS(BaseAgentTestCase):
         agent.port_update(mock.Mock(), **kwargs)
 
     def test_port_update(self):
-        port = {'id': 1, 'security_groups': 'default'}
+        port = {'id': '1', 'security_groups': 'default'}
 
         with mock.patch.object(self.ovsbridge.return_value,
                                'get_vif_port_by_id',
-                               return_value=1) as get_vif:
+                               return_value='1') as get_vif:
             self.mock_port_update(port=port)
 
-        get_vif.assert_called_once_with(1)
+        get_vif.assert_called_once_with('1')
         self.sg_agent.assert_has_calls([
             mock.call().refresh_firewall()
         ])
 
     def test_port_update_not_vifport(self):
-        port = {'id': 1, 'security_groups': 'default'}
+        port = {'id': '1', 'security_groups': 'default'}
 
         with mock.patch.object(self.ovsbridge.return_value,
                                'get_vif_port_by_id',
-                               return_value=0) as get_vif:
+                               return_value=False) as get_vif:
             self.mock_port_update(port=port)
 
-        get_vif.assert_called_once_with(1)
+        get_vif.assert_called_once_with('1')
         self.assertFalse(self.sg_agent.return_value.refresh_firewall.called)
 
     def test_port_update_without_secgroup(self):
-        port = {'id': 1}
+        port = {'id': '1'}
 
         with mock.patch.object(self.ovsbridge.return_value,
                                'get_vif_port_by_id',
-                               return_value=1) as get_vif:
+                               return_value='1') as get_vif:
             self.mock_port_update(port=port)
 
-        get_vif.assert_called_once_with(1)
+        get_vif.assert_called_once_with('1')
         self.assertFalse(self.sg_agent.return_value.refresh_firewall.called)
 
     def mock_update_ports(self, vif_port_set=None, registered_ports=None):
+        vif_port_set = vif_port_set or set()
+        registered_ports = registered_ports or set()
         with mock.patch.object(self.ovsbridge.return_value,
                                'get_vif_port_set',
                                return_value=vif_port_set):
@@ -103,11 +107,11 @@ class TestRestProxyAgentOVS(BaseAgentTestCase):
         self.assertIsNone(self.mock_update_ports())
 
     def test_update_ports_changed(self):
-        vif_port_set = set([1, 3])
-        registered_ports = set([1, 2])
+        vif_port_set = set(['1', '3'])
+        registered_ports = set(['1', '2'])
         expected = dict(current=vif_port_set,
-                        added=set([3]),
-                        removed=set([2]))
+                        added=set(['3']),
+                        removed=set(['2']))
 
         actual = self.mock_update_ports(vif_port_set, registered_ports)
 
@@ -183,3 +187,130 @@ class TestRestProxyAgent(BaseAgentTestCase):
             mock.call('integ_br', 5, 'ovs'),
             mock.call().daemon_loop()
         ])
+
+
+class TestRestProxyAgentIVS(TestRestProxyAgentOVS):
+
+    def setUp(self):
+        super(TestRestProxyAgentIVS, self).setUp()
+        # we don't want to mock out the whole class, just the part that
+        # tries to run commands on the system
+        self.ovsbridge_p.stop()
+        self.runvsctl = mock.patch(IVSBRIDGE + '.run_vsctl').start()
+
+    def mock_agent(self):
+        mock_context = mock.Mock(return_value='abc')
+        self.context.get_admin_context_without_session = mock_context
+        # same as OVS case except passing 'ivs' for vswitch type
+        return self.mod_agent.RestProxyAgent('int-br', 2, vs='ivs')
+
+    def mock_update_ports(self, vif_port_set=None, registered_ports=None):
+        vif_port_set = vif_port_set or set()
+        registered_ports = registered_ports or set()
+        agent = self.mock_agent()
+        with mock.patch.object(agent.int_br,
+                               'get_vif_port_set',
+                               return_value=vif_port_set):
+            return agent._update_ports(registered_ports)
+
+    def test_port_update_not_vifport(self):
+        port = {'id': '1', 'security_groups': 'default'}
+
+        with mock.patch(IVSBRIDGE + '.get_vif_port_by_id',
+                        return_value=False) as get_vif:
+            self.mock_port_update(port=port)
+
+        get_vif.assert_called_once_with('1')
+        self.assertFalse(self.sg_agent.return_value.refresh_firewall.called)
+
+    def test_port_update_without_secgroup(self):
+        port = {'id': '1'}
+
+        with mock.patch(IVSBRIDGE + '.get_vif_port_by_id',
+                        return_value='1') as get_vif:
+            self.mock_port_update(port=port)
+
+        get_vif.assert_called_once_with('1')
+        self.assertFalse(self.sg_agent.return_value.refresh_firewall.called)
+
+    def test_port_update(self):
+        port = {'id': '1', 'security_groups': 'default'}
+
+        with mock.patch(IVSBRIDGE + '.get_vif_port_by_id',
+                        return_value='1') as get_vif:
+            self.mock_port_update(port=port)
+
+        get_vif.assert_called_once_with('1')
+        self.sg_agent.assert_has_calls([
+            mock.call().refresh_firewall()
+        ])
+
+    def test_port_list_with_new_method(self):
+        agent = self.mock_agent()
+        self.runvsctl.return_value = "port1\nport2\nport3\n"
+        self.assertEqual(['port1', 'port2', 'port3'],
+                         agent.int_br.get_port_name_list())
+        expected_calls = [mock.call(['list-ports'], True)]
+        self.assertEqual(expected_calls, self.runvsctl.mock_calls)
+
+    def test_port_list_fallback_to_show(self):
+        agent = self.mock_agent()
+        # fail the first call to 'list-ports' so it falls back to 'show'
+        self.runvsctl.side_effect = [RuntimeError(), IVS_SHOW_OUTPUT]
+        self.assertEqual(['ivs', 'p1p1', 'p1p2', 'os-mgmt',
+                          'tapa40e6816-82', 'inband'],
+                         agent.int_br.get_port_name_list())
+        expected_calls = [mock.call(['list-ports'], True),
+                          mock.call(['show'], True)]
+        self.assertEqual(expected_calls, self.runvsctl.mock_calls)
+
+
+# some test 'ivs-ctl show' data
+IVS_SHOW_OUTPUT = '''
+ovs-system:
+  kernel lookups: hit=7 missed=11 lost=0
+  kernel flows=0
+  ports:
+    0 ovs-system (internal)
+      rx: packets=0 bytes=0 errors=0 dropped=0
+      tx: packets=0 bytes=0 errors=0 dropped=0
+    1 bond0
+      rx: packets=0 bytes=0 errors=0 dropped=0
+      tx: packets=0 bytes=0 errors=0 dropped=0
+    2 br-int (internal)
+      rx: packets=0 bytes=0 errors=0 dropped=0
+      tx: packets=0 bytes=0 errors=0 dropped=0
+    3 vxlan_sys_4789 (unknown vport type)
+      rx: packets=0 bytes=0 errors=0 dropped=0
+      tx: packets=0 bytes=0 errors=0 dropped=0
+    4 br-tun (internal)
+      rx: packets=0 bytes=0 errors=0 dropped=0
+      tx: packets=0 bytes=0 errors=0 dropped=0
+    5 br-eth0 (internal)
+      rx: packets=0 bytes=0 errors=0 dropped=0
+      tx: packets=0 bytes=0 errors=0 dropped=0
+    6 br-ex (internal)
+      rx: packets=8 bytes=648 errors=0 dropped=0
+      tx: packets=0 bytes=0 errors=0 dropped=0
+ivs:
+  kernel lookups: hit=19446 missed=2998 lost=1
+  kernel flows=1
+  ports:
+    0 ivs (internal)
+      rx: packets=0 bytes=0 errors=0 dropped=0
+      tx: packets=0 bytes=0 errors=0 dropped=0
+    1 p1p1
+      rx: packets=1863 bytes=232066 errors=0 dropped=0
+      tx: packets=1210 bytes=100440 errors=0 dropped=0
+    2 p1p2
+      rx: packets=0 bytes=0 errors=0 dropped=0
+      tx: packets=0 bytes=0 errors=0 dropped=0
+    3 os-mgmt (internal)
+      rx: packets=8 bytes=648 errors=0 dropped=0
+      tx: packets=0 bytes=0 errors=0 dropped=0
+    4 tapa40e6816-82
+      rx: packets=8 bytes=648 errors=0 dropped=0
+      tx: packets=0 bytes=0 errors=0 dropped=0
+    1000 inband (internal)
+      rx: packets=20565 bytes=1768550 errors=0 dropped=0
+      tx: packets=586 bytes=50368 errors=0 dropped=0'''
