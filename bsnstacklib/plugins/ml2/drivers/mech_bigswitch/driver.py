54f60ac5a69737339abc34e6233785bc9149da6b
Kanzhe Jiang <kanzhe@gmail.com>
2015-03-06 14:42:55 -0800

    Update to BSN l3 plugin
    include tenant_id to router's external_gateway_info, include routers in topology, skip router gateway and interface ports
    
    Change-Id: I1d5997e6a3c82def24d254063ade088f13e76e18

--- bsnstacklib/plugins/ml2/drivers/mech_bigswitch/driver.py
+++ bsnstacklib/plugins/ml2/drivers/mech_bigswitch/driver.py
@@ -58,10 +58,7 @@ class BigSwitchMechanismDriver(plugin.NeutronRestProxyV2Base,
 
         # init network ctrl connections
         self.servers = servermanager.ServerPool()
-        self.servers.get_topo_function = self._get_all_data
-        self.servers.get_topo_function_args = {'get_ports': True,
-                                               'get_floating_ips': False,
-                                               'get_routers': False}
+        self.servers.get_topo_function = self._get_all_data_auto
         self.segmentation_types = ', '.join(cfg.CONF.ml2.type_drivers)
         # Track hosts running IVS to avoid excessive calls to the backend
         self.ivs_host_cache = {}
