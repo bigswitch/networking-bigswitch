fd3bd29cb4777a0e03cad51e7bf82c627b4d37eb
Kanzhe Jiang <kanzhe@gmail.com>
2015-05-05 15:56:30 -0700

    Remove call to _extend_port_dict_binding to avoid vif_type overwrite
    Conflicts:
    	bsnstacklib/plugins/ml2/drivers/mech_bigswitch/driver.py
    
    Change-Id: I23b9f369dfe4ca580054dfee1566549f99024772

--- bsnstacklib/plugins/ml2/drivers/mech_bigswitch/driver.py
+++ bsnstacklib/plugins/ml2/drivers/mech_bigswitch/driver.py
@@ -186,10 +186,8 @@ class BigSwitchMechanismDriver(plugin.NeutronRestProxyV2Base,
         port = copy.deepcopy(context.current)
         net = context.network.current
         port['network'] = net
-        port['bound_segment'] = context.top_bound_segment
-        actx = ctx.get_admin_context()
-        prepped_port = self._extend_port_dict_binding(actx, port)
-        prepped_port = self._map_state_and_status(prepped_port)
+        port['bound_segment'] = context.bound_segment
+        prepped_port = self._map_state_and_status(port)
         if (portbindings.HOST_ID not in prepped_port or
             prepped_port[portbindings.HOST_ID] == ''):
             LOG.warning(_LW("Ignoring port notification to controller because "
