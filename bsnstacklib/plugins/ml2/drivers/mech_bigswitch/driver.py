22fb5057db31242f95d9bc4b9842a2a361d01caf
Kanzhe Jiang <kanzhe@gmail.com>
2015-06-28 00:24:46 -0700

    listen project creation/deletion and create default security-group
    Change-Id: Icf95ec6e0d5ef6965dfedeb7b325ff64f27978ec

--- bsnstacklib/plugins/ml2/drivers/mech_bigswitch/driver.py
+++ bsnstacklib/plugins/ml2/drivers/mech_bigswitch/driver.py
@@ -100,9 +100,11 @@ class BigSwitchMechanismDriver(plugin.NeutronRestProxyV2Base,
         # the event type and determine what to do from there.
         target = oslo.messaging.Target(topic='notifications',
                                        server=cfg.CONF.host)
+        keystone_target = oslo.messaging.Target(
+             topic='notifications', exchange='keystone', server=cfg.CONF.host)
         self.listener = oslo.messaging.get_notification_listener(
-            n_rpc.TRANSPORT, [target], [self], executor='eventlet',
-            allow_requeue=False)
+            n_rpc.TRANSPORT, [target, keystone_target], [self],
+            executor='eventlet', allow_requeue=False)
         self.listener.start()
 
     def info(self, ctxt, publisher_id, event_type, payload, metadata):
@@ -113,6 +115,14 @@ class BigSwitchMechanismDriver(plugin.NeutronRestProxyV2Base,
         elif event_type == 'security_group.delete.end':
             LOG.debug("Security group deleted: %s" % payload)
             self.bsn_delete_security_group(payload['security_group_id'])
+        elif event_type == 'identity.project.deleted':
+            LOG.debug("Project deleted: %s" % payload)
+            self.bsn_delete_tenant(payload['resource_info'])
+        elif event_type == 'identity.project.created':
+            LOG.debug("Project created: %s" % payload)
+            self.bsn_create_tenant(payload['resource_info'])
+        else:
+            LOG.debug("Else events: %s payload: %s" % (event_type, payload))
 
     @put_context_in_serverpool
     def security_groups_rule_updated(self, context, **kwargs):
