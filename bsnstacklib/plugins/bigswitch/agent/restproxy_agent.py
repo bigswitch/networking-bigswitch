57c6946134a5a399e23aa2f5eee16a60463965ff
xinwu <xin.wu@bigswitch.com>
2015-08-21 16:36:00 -0700

    Revert "No more depend on portbindings vendor vif type constants"
    This reverts commit 92dd01f1c695648b888b615a5c31e50f5ef4d1a9.
    
    Revert "Fixes for a couple of upstream refactors"
    
    This reverts commit 5309c3fbda331b685192c260c740bcdf14aa2833.
    
    Revert "update get_port_and_sgs to new signature"
    
    This reverts commit 7a62980d2b78f0edff9e60b88fc5333f7069565e.
    
    Change-Id: Ia9d764ccab141bde5b2d9c83649db7c8cd131324

--- bsnstacklib/plugins/bigswitch/agent/restproxy_agent.py
+++ bsnstacklib/plugins/bigswitch/agent/restproxy_agent.py
@@ -25,7 +25,6 @@ eventlet.monkey_patch()
 from oslo_config import cfg
 from oslo_log import log
 import oslo_messaging
-from oslo_service import loopingcall
 from oslo_utils import excutils
 
 from neutron.agent.common import ovs_lib
@@ -38,6 +37,7 @@ from neutron.common import topics
 from neutron import context as q_context
 from neutron.extensions import securitygroup as ext_sg
 from neutron.i18n import _LE, _LI
+from neutron.openstack.common import loopingcall
 
 from bsnstacklib.plugins.bigswitch import config as pl_config
 
