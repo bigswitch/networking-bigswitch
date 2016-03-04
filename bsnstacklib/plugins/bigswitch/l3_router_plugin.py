29f928a0e6a0c286fb4ca940c3a9fbb37c6e2f42
Kanzhe Jiang <kanzhe@gmail.com>
2016-03-04 07:00:05 -0800

    Fix deprecation error
    Change-Id: I41d0565dd5a9369772315af4f880c62160fdb963

--- bsnstacklib/plugins/bigswitch/l3_router_plugin.py
+++ bsnstacklib/plugins/bigswitch/l3_router_plugin.py
@@ -31,7 +31,7 @@ from neutron.api import extensions as neutron_extensions
 from neutron.common import exceptions
 from neutron.db import l3_db
 from neutron.extensions import l3
-from neutron.i18n import _LE
+from neutron._i18n import _, _LE
 from neutron import manager
 from neutron.plugins.common import constants
 
