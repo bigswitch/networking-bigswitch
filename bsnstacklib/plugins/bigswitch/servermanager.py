bdc592e3019d650624aea4f067a5fc562afa187b
Sarath Kumar <sarath.kutty@bigswitch.com>
2016-09-30 17:47:39 -0700

    BVS-7099: raise exception on topology sync failures
    Today topology sync failures go under the radar. Here we log it and raise
    exception just like every other REST call failure
    
    Change-Id: I816470763955f2db61b3e9e70132083b7b8da518

--- bsnstacklib/plugins/bigswitch/servermanager.py
+++ bsnstacklib/plugins/bigswitch/servermanager.py
@@ -584,15 +584,23 @@ class ServerPool(object):
                     raise cfg.Error(_('Server requires synchronization, '
                                       'but no topology function was defined.'))
 
+                LOG.info(_LI("ServerProxy: HashConflict detected with request "
+                             "%(action)s %(resource)s Starting Topology sync"),
+                         {'action': action, 'resource': resource})
                 self._topo_sync_in_progress = True
                 eventlet.spawn_n(self.keep_updating_lock)
                 try:
                     data = self.get_topo_function(
                                **self.get_topo_function_args)
                     if data:
-                        active_server.rest_call('POST', TOPOLOGY_PATH, data,
-                                                timeout=None)
+                        ret_ts = active_server.rest_call('POST', TOPOLOGY_PATH,
+                                                         data, timeout=None)
+                        if self.server_failure(ret_ts, ignore_codes):
+                            LOG.error(_LE("ServerProxy: Topology sync failed"))
+                            raise RemoteRestError(reason=ret_ts[2],
+                                                  status=ret_ts[0])
                 finally:
+                    LOG.info(_LI("ServerProxy: Topology sync completed"))
                     self._topo_sync_in_progress = False
                     if data is None:
                         return None
