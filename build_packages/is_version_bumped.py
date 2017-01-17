eb80152363294d0e4689922aa354cf99b46e88b3
Anh Tran <anhtt@vn.fujitsu.com>
2017-01-17 09:44:29 +0700

    Remove white space between print ()
    Change-Id: Ie70a62027d304fdad11d236ba3973c70a3278e68

--- build_packages/is_version_bumped.py
+++ build_packages/is_version_bumped.py
@@ -26,9 +26,9 @@ if 'version' not in two_lines:
 lines = str(two_lines).split('\n')
 version1 = StrictVersion(lines[0].split('=')[1].strip())
 version2 = StrictVersion(lines[1].split('=')[1].strip())
-print ('version1: ', version1)
-print ('version2: ', version2)
+print('version1: ', version1)
+print('version2: ', version2)
 if version2 > version1:
-    print ('Version update correct.')
+    print('Version update correct.')
 else:
     sys.exit("new version string is < old version string. Build FAILED")
