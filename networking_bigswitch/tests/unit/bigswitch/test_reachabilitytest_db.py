a2b44d133f3054d634ef36c3bd76c3c079c26126
Aditya Vaja <wolverine.av@gmail.com>
2017-02-10 07:11:26 -0800

    OSP-21: rename bsnstacklib to networking_bigswitch
     - this is to bring bsn in sync with upstream naming convention
     - fixes merge failure
    
    Conflicts:
    	networking_bigswitch/tests/unit/bigswitch/test_networktemplate_db.py
    	networking_bigswitch/tests/unit/bigswitch/test_networktemplateassignment_db.py
    	networking_bigswitch/tests/unit/bigswitch/test_reachabilityquicktest_db.py
    	networking_bigswitch/tests/unit/bigswitch/test_reachabilitytest_db.py
    	networking_bigswitch/tests/unit/bigswitch/test_router_db.py
    	networking_bigswitch/tests/unit/bigswitch/test_servermanager.py
    	setup.cfg
    
    Change-Id: I71cbcc2bb427cd10908e9f2dee090a00a1ea8c5b

---
+++ networking_bigswitch/tests/unit/bigswitch/test_reachabilitytest_db.py
@@ -0,0 +1,65 @@
+# Copyright 2016 Big Switch Networks, Inc.  All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# Adapted from neutron.tests.unit.db.test_agents_db.py
+
+from networking_bigswitch.plugins.bigswitch.db import reachability_test_db
+from neutron import context
+from neutron.tests.unit import testlib_api
+
+
+class TestReachabilityTestDbMixin(testlib_api.SqlTestCase):
+    def setUp(self):
+        super(TestReachabilityTestDbMixin, self).setUp()
+        self.context = context.get_admin_context()
+        self.dbmixin = reachability_test_db.ReachabilityTestDbMixin()
+
+    def _assert_ref_fields_are_equal(self, reference, result):
+        """Compare (key, value) pairs of a reference dict with the result
+
+           Note: the result MAY have additional keys
+        """
+
+        for field, value in reference.items():
+            self.assertEqual(value, result[field], field)
+
+    def test_create_reachabilitytest(self):
+        reachabilitytest_dict = {
+            'reachabilitytest': {
+                'tenant_id': 'admin_tenant_id',
+                'name': 'test1',
+                'src_tenant_name': 'admin',
+                'src_segment_name': 'web',
+                'src_ip': '10.1.1.2',
+                'dst_ip': '10.2.1.2',
+                'expected_result': 'dropped'
+            }
+        }
+
+        reachabilitytest = self.dbmixin.create_reachabilitytest(
+            self.context, reachabilitytest_dict)
+        self._assert_ref_fields_are_equal(
+            reachabilitytest_dict['reachabilitytest'], reachabilitytest)
+
+    def test_get_reachabilitytest(self):
+        pass
+
+    def test_get_reachabilitytests(self):
+        pass
+
+    def test_update_reachabilitytest(self):
+        pass
+
+    def test_delete_reachabilitytest(self):
+        pass
