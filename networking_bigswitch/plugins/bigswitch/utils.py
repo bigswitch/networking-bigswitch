31c45a580b453828dc43d44c1ff11b06cc36a6bf
Aditya Vaja <wolverine.av@gmail.com>
2017-04-18 12:50:39 -0700

    BVS-5916: ensure special chars are handled for tenant as well
     - headers for the REST call to the controller contains the
       tenant name, which needs replacing
     - local name cache for tenants was missing the replacement
    
    Change-Id: I29c17f614a8c499f2649ce23e786584a128ea50d
    (cherry picked from commit d22a8df256febdc378c9094c80ae9cdeed85b1d4)

---
+++ networking_bigswitch/plugins/bigswitch/utils.py
@@ -0,0 +1,39 @@
+# Copyright 2017 Big Switch Networks, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class Util(object):
+    """
+    Placeholder for static methods that can be called from across the plugin
+    and reused as required.
+    """
+
+    @staticmethod
+    def format_resource_name(name):
+        """
+        Util method to format resource names to make them compatible with BCF.
+
+        Replaces special characters with its corresponding BCF compatible
+        encoding.
+
+        :param name non empty string which is the name of the resource
+        :rtype string name with special characters replaced
+        """
+        return (name
+                # always replace underscores first, since other replacements
+                # contain underscores as part of replacement
+                .replace('_', '__')
+                .replace(' ', '_s')
+                .replace('\'', '_a'))
