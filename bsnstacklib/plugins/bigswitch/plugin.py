a0f54a47ac0c7b6255b18343591632dc33a9a28e
Aditya Vaja <wolverine.av@gmail.com>
2017-05-08 16:33:21 -0700

    OSP-72: cleanup popping of subnet name
     - remove from the base method for getting subnets instead of
       post the call
     - this saves the router_update that appends subnets but didn't
       remove the name
    
    Change-Id: Ic7456cf1c5437a54366b33fc9bdab0b835a844c2
    (cherry picked from commit 231b4b91b1d07c5a4adb9b975297b7d83582e184)

--- bsnstacklib/plugins/bigswitch/plugin.py
+++ bsnstacklib/plugins/bigswitch/plugin.py
@@ -228,11 +228,6 @@ class NeutronRestProxyV2Base(db_base_plugin_v2.NeutronDbPluginV2,
                         new_subnets.append(subnet)
                     mapped_network['subnets'] = new_subnets
 
-                # OSP-45: remove name to avoid NSAPI error in convertToAscii
-                if 'subnets' in mapped_network:
-                    for subnet in mapped_network['subnets']:
-                        subnet.pop('name', None)
-
                 flips_n_ports = mapped_network
                 if get_floating_ips:
                     flips_n_ports = self._get_network_with_floatingips(
@@ -495,6 +490,9 @@ class NeutronRestProxyV2Base(db_base_plugin_v2.NeutronDbPluginV2,
             context, network['id'])
         # include ML2 segmentation types
         network['segmentation_types'] = getattr(self, "segmentation_types", "")
+        # OSP-45: remove name to avoid NSAPI error in convertToAscii
+        for subnet in (subnets or []):
+            subnet.pop('name', None)
         return network
 
     def _send_create_network(self, network, context=None):
@@ -509,10 +507,7 @@ class NeutronRestProxyV2Base(db_base_plugin_v2.NeutronDbPluginV2,
             self.bsn_create_security_group(sg=default_group[0])
         mapped_network = self._get_mapped_network_with_subnets(network,
                                                                context)
-        # OSP-45: remove name to avoid NSAPI error in convertToAscii
-        if 'subnets' in mapped_network:
-            for subnet in mapped_network['subnets']:
-                subnet.pop('name', None)
+
         if not tenant_id:
             tenant_id = servermanager.SERVICE_TENANT
             mapped_network['tenant_id'] = servermanager.SERVICE_TENANT
@@ -527,10 +522,6 @@ class NeutronRestProxyV2Base(db_base_plugin_v2.NeutronDbPluginV2,
         tenant_id = network['tenant_id']
         mapped_network = self._get_mapped_network_with_subnets(network,
                                                                context)
-        # OSP-45: remove name to avoid NSAPI error in convertToAscii
-        if 'subnets' in mapped_network:
-            for subnet in mapped_network['subnets']:
-                subnet.pop('name', None)
         net_fl_ips = self._get_network_with_floatingips(mapped_network,
                                                         context)
         if not tenant_id:
