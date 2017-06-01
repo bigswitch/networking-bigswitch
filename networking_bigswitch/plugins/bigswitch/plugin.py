82f7f0d2ba69940b5500ed6beeded21876a19d65
Joe Mills <joseph.mills@bigswitch.com>
2017-06-01 09:33:36 +0900

    use neutron_lib for extra_dhcp_opts
    Change-Id: I8942ef8521aecd0aa53d73e826953e925e2b72ff

--- networking_bigswitch/plugins/bigswitch/plugin.py
+++ networking_bigswitch/plugins/bigswitch/plugin.py
@@ -75,10 +75,10 @@ from neutron.db import models_v2
 from neutron.db import securitygroups_rpc_base as sg_db_rpc
 from neutron.extensions import allowedaddresspairs as addr_pair
 from neutron.extensions import external_net
-from neutron.extensions import extra_dhcp_opt as edo_ext
 from neutron.extensions import l3
 from neutron.plugins.common import constants as pconst
 
+from neutron_lib.api.definitions import extra_dhcp_opt as edo_ext
 from neutron_lib.api.definitions import portbindings
 from neutron_lib import constants as const
 from neutron_lib import context as qcontext
