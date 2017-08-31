4f17d43f1bfabd232eb1f97f1b7f125fb6e63b8f
Aditya Vaja <wolverine.av@gmail.com>
2017-08-31 07:22:55 -0700

    disable default LLDP on x710 interfaces
     - identify interfaces that are x710 NICs and stop the default
       LLDPs being sent on that interface
     - we still add the interface to the uplink list and send BSN
       specific LLDPs
    
    Change-Id: I3402957aa3a6aa2e7c75057103d1bf89d00293af

--- networking_bigswitch/bsnlldp/send_lldp.py
+++ networking_bigswitch/bsnlldp/send_lldp.py
@@ -50,6 +50,13 @@ TTL = 120
 INTERVAL = 10
 CHASSIS_ID_LOCALLY_ASSIGNED = 7
 PORT_ID_INTERFACE_ALIAS = 1
+PCI_IDS_DIR = "/run/pci_ids"
+LLDP_START_STR = "lldp start"
+LLDP_STOP_STR = "lldp stop"
+X710_INTEL_DRIVER_STR = "i40e"
+
+# read and save lldp status for different interfaces
+lldp_status = {}
 
 
 class struct_sockaddr(Structure):
@@ -195,6 +202,13 @@ def get_mac_str(network_interface):
     with open("/sys/class/net/%s/address" % network_interface) as f:
         return f.read().strip()
 
+def readfile(path):
+    with open(path) as f:
+        return f.read()
+
+def writefile(path, data):
+    with open(path, "w") as f:
+        f.write(data)
 
 def lldp_ethertype():
     return raw_bytes_of_int(LLDP_ETHERTYPE, 2, "LLDP ethertype")
@@ -293,6 +307,122 @@ def daemonize():
         # Exit from the 2nd parent process
         os._exit(os.EX_OK)
 
+def list_a_minus_list_b(list_a, list_b):
+    """
+    This method assumes input is two lists with unique elements aka sets. It
+    then returns all unique elements in list_a, not present in list_b.
+
+    :param list_a:
+    :param list_b:
+    :return: list of elements only in list_a OR an empty list
+    """
+    return list(set(list_a) - set(list_b))
+
+def find_pci_id(uplink):
+    if os.path.exists("/sys/bus/pci/devices/%s" % uplink):
+        return uplink
+
+    stash = os.path.join(PCI_IDS_DIR, uplink)
+    if os.path.exists(stash):
+        pci_id = readfile(stash)
+        return pci_id
+
+    if not os.path.exists("/sys/class/net/%s" % uplink):
+        raise RuntimeError("No such network device %s" % uplink)
+
+    pci_id = os.path.basename(os.readlink("/sys/class/net/%s/device" % uplink))
+
+    if not os.path.exists(PCI_IDS_DIR):
+        os.mkdir(PCI_IDS_DIR)
+
+    writefile(stash, pci_id)
+    return pci_id
+
+def save_x710_intf_lldp_status(intf):
+    """
+    This will read the LLDP status being sent for the interface, only if it is
+    an x710 network card.
+
+    If its status is already recorded, it returns True
+    If not, it will read and save and return True for x710 interfaces
+    For all other interfaces, it will return False
+
+    :param intf:
+    :return: (boolean, pci_id) True or False based on outcome. pci_id is None
+                               if False. Actual pci_id otherwise
+    """
+    LOG.syslog("Read and save LLDP Tx status from device as needed")
+
+    uplink = intf.strip()
+    pci_id = find_pci_id(uplink)
+    LOG.syslog("Uplink %(uplink)s is PCI device %(pci_id)s" %
+               {"uplink": uplink, "pci_id": pci_id})
+
+    if uplink in lldp_status:
+        LOG.syslog("Uplink %s already has LLDP status saved as %s" %
+                   (uplink, lldp_status[uplink]))
+        return (True, pci_id)
+
+    # check the card type, if x710, read lldp statusfrom nic
+    vendor = int(readfile("/sys/bus/pci/devices/%s/vendor" % pci_id), 16)
+    device = int(readfile("/sys/bus/pci/devices/%s/device" % pci_id), 16)
+
+    LOG.syslog("pci_id %s vendor %#04x device %#04x" %
+               (pci_id, vendor, device))
+
+    # check if X710 NIC, if yes, read lldp status
+    if (vendor == 0x8086 and device == 0x1572):
+        # default assume status as lldp is stopped
+        lldp_status[uplink] = LLDP_STOP_STR
+        if os.path.exists("/sys/bus/pci/devices/%s/driver" % pci_id):
+            driver = os.path.basename(
+                os.readlink("/sys/bus/pci/devices/%s/driver" % pci_id))
+            if driver == X710_INTEL_DRIVER_STR:
+                status = readfile("/sys/kernel/debug/%s/%s/command" % (driver, pci_id))
+                # update the status
+                if (status is LLDP_START_STR or status is LLDP_STOP_STR):
+                    lldp_status[uplink] = status
+                LOG.syslog("LLDP status read for Uplink %s pci_id %s "
+                           "vendor %#04x device %#04x" %
+                           (uplink, pci_id, vendor, device))
+                return (True, pci_id)
+        else:
+            LOG.syslog("LLDP status not read for Uplink %s pci_id %s "
+                       "vendor %#04x device %#04x as driver file "
+                       "doesn't exist" % (uplink, pci_id, vendor, device))
+    return (False, None)
+
+def update_x710_lldp_status(pci_id, status):
+    """
+    This method should only be called when interface has been identified as
+    x710 interface.
+
+    :param pci_id:
+    :param status:
+    :return: None
+    """
+    writefile("/sys/kernel/debug/%s/%s/command"
+              % (X710_INTEL_DRIVER_STR, pci_id),
+              status)
+
+def save_and_stop_x710_intf_lldp(intf):
+    """
+    This checks if the interface is x710 interface, if yes, it will save the
+    status in a local variable and stop LLDP on that interface.
+
+    :param intf:
+    :return: True, if its x710 interface, False otherwise
+    """
+    is_x710, pci_id = save_x710_intf_lldp_status(intf)
+    if is_x710:
+        update_x710_lldp_status(pci_id, LLDP_STOP_STR)
+        return True
+    return False
+
+def save_and_restore_x710_intf_lldp(intf):
+    is_x710, pci_id = save_x710_intf_lldp_status(intf)
+    if is_x710:
+        update_x710_lldp_status(pci_id, lldp_status[intf.strip()])
 
 def send_lldp():
     args = parse_args()
@@ -336,6 +466,10 @@ def send_lldp():
     LOG.syslog("LLDP interfaces are %s" % ','.join(intfs))
     LOG.syslog("LLDP chassisid is %s" % chassisid)
 
+    # save lldp status of x710 interfaces and update it to stop
+    for intf in intfs:
+        save_and_stop_x710_intf_lldp(intf)
+
     senders, frames = _generate_senders_frames(intfs, chassisid, args)
     interval = INTERVAL
     if args.interval:
@@ -346,6 +480,19 @@ def send_lldp():
             # refresh interface list, since a new link may have come up
             new_intfs, new_chassisid = get_uplinks_and_chassisid()
             if (intfs, chassisid) != (new_intfs, new_chassisid):
+
+                # restore lldp for x710 devices that are no longer uplinks
+                non_uplink_intfs = list_a_minus_list_b(list_a=intfs,
+                                                       list_b=new_intfs)
+                for intf in non_uplink_intfs:
+                    save_and_restore_x710_intf_lldp(intf)
+
+                # stop lldp for x710 interfaces that are uplinks
+                new_uplink_intfs = list_a_minus_list_b(list_a=new_intfs,
+                                                       list_b=intfs)
+                for intf in new_uplink_intfs:
+                    save_and_stop_x710_intf_lldp(intf)
+
                 # something changed, update it
                 LOG.syslog("LLDP interfaces updated from %(old_intfs)s"
                            " to %(new_intfs)s" %
