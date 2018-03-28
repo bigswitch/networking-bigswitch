0c8a4f2466260df864ec0c2d651da5bb96e19937
Aditya Prakash Vaja <wolverine.av@gmail.com>
2018-03-28 13:25:57 -0700

    topics moved from neutron.common to neutron_lib.agent
    Change-Id: If12a55b75e17ed46f99cfeadf86d0b8a82894020

--- networking_bigswitch/plugins/bigswitch/plugin.py
+++ networking_bigswitch/plugins/bigswitch/plugin.py
@@ -60,7 +60,6 @@ from neutron.api.rpc.handlers import dhcp_rpc
 from neutron.api.rpc.handlers import metadata_rpc
 from neutron.api.rpc.handlers import securitygroups_rpc
 from neutron.common import rpc as n_rpc
-from neutron.common import topics
 from neutron.db import agents_db
 from neutron.db import agentschedulers_db
 from neutron.db import allowedaddresspairs_db as addr_pair_db
@@ -73,6 +72,7 @@ from neutron.db.models import securitygroup as sg_db
 from neutron.db import models_v2
 from neutron.db import securitygroups_rpc_base as sg_db_rpc
 
+from neutron_lib.agent import topics
 from neutron_lib.api.definitions import allowedaddresspairs as addr_pair
 from neutron_lib.api.definitions import external_net as extnet_apidef
 from neutron_lib.api.definitions import extra_dhcp_opt as edo_ext
