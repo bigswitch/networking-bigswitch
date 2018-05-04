7acf24b48277cf150eccaef5e7282ec70876bd8b
Aditya Prakash Vaja <wolverine.av@gmail.com>
2018-05-04 13:12:35 -0700

    remove hash_handler check and topo_sync
     - changes consistency_watchdog to force_topo_sync each time
     - interval of consistency_watchdog = 60 * 30 (30 mins)
     - other places where topo_sync is forced:
       - DELETE operation fails
       - tenant cache outdated
    
    Change-Id: I83be81137bd8d0acd990c711b8f9758e5b2fe4b1

--- networking_bigswitch/plugins/bigswitch/bsn_service_plugin.py
+++ networking_bigswitch/plugins/bigswitch/bsn_service_plugin.py
@@ -259,3 +259,11 @@ class BSNServicePlugin(service_base.ServicePluginBase,
     def delete_reachabilityquicktest(self, context, id):
         self.reachabilityquicktest_db_mixin.delete_reachabilityquicktest(
             context=context, id=id)
+
+    # public CRUD methods for Topology Sync command
+    def get_forcesynctopologies(self, context, filters=None, fields=None,
+                                sorts=None, limit=None, marker=None,
+                                page_reverse=False):
+        self.servers.force_topo_sync(check_ts=False)
+        return {'id': '1', 'project_id': context.project_id,
+                'result': 'SUCCESS'}
--- networking_bigswitch/plugins/bigswitch/db/consistency_db.py
+++ networking_bigswitch/plugins/bigswitch/db/consistency_db.py
@@ -13,9 +13,9 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 import eventlet
+import datetime
 import random
 import re
-import string
 import time
 
 from oslo_config import cfg
@@ -37,6 +37,7 @@ MAX_LOCK_WAIT_TIME = 15  # seconds
 MAX_LOCK_TOPOSYNC_WAIT_TIME = 300  # seconds
 MIN_LOCK_RETRY_SLEEP_TIME = 100  # milliseconds
 MAX_LOCK_RETRY_SLEEP_TIME = 250  # milliseconds
+TOPO_SYNC_EXPIRED_SECS = 1800
 DBLOCK_ID_LEN = 12
 DBLOCK_PREFIX_TOPO = "TOPO"
 DBLOCK_PREFIX_AUTOGEN = "A"
@@ -72,6 +73,12 @@ def clear_db():
     HashHandler._FACADE = None
 
 
+def convert_ts_to_datetime(timestamp_ms):
+    dt_string = datetime.datetime.fromtimestamp(float(timestamp_ms)).strftime(
+        '%Y-%m-%d %H:%M:%S.%f')
+    return dt_string
+
+
 class HashHandler(object):
     '''
     A wrapper object to keep track of the session between the read
@@ -83,23 +90,16 @@ class HashHandler(object):
     '''
     _FACADE = None
 
-    def __init__(self, hash_id='1', prefix=None):
+    def __init__(self, hash_id='1', timestamp_ms=None):
         if HashHandler._FACADE is None:
             HashHandler._FACADE = session.EngineFacade.from_config(
                 cfg.CONF, sqlite_fk=True)
 
-        if not prefix:
-            prefix = DBLOCK_PREFIX_AUTOGEN
-        length = max((DBLOCK_ID_LEN - len(prefix)), 0)
-
         self.hash_id = hash_id
         self.session = HashHandler._FACADE.get_session(autocommit=True,
                                                        expire_on_commit=False)
-        self.random_lock_id = ''.join(random.choice(string.ascii_uppercase
-                                                    + string.digits)
-                                      for _ in range(length))
-        self.random_lock_id = prefix + self.random_lock_id
-        self.lock_marker = 'LOCKED_BY[%s]' % self.random_lock_id
+        self.lock_ts = str(timestamp_ms) if timestamp_ms else str(time.time())
+        self.lock_marker = 'LOCKED_BY[%s]' % self.lock_ts
 
     def _get_current_record(self):
         with self.session.begin(subtransactions=True):
@@ -140,7 +140,7 @@ class HashHandler(object):
         return result.rowcount != 0
 
     def _get_lock_owner(self, record):
-        matches = re.findall(r"^LOCKED_BY\[(\w+)\]", record)
+        matches = re.findall(r"^LOCKED_BY\[(\d+\.\d+)\]", record)
         if not matches:
             return None
         return matches[0]
@@ -150,7 +150,7 @@ class HashHandler(object):
         @return: DB_HASH, on success
                  None, otherwise
         """
-        lock_id = self.random_lock_id
+        lock_id = self.lock_ts
         current_lock_owner = self._get_lock_owner(res.hash)
         LOG.warning(_LW("Gave up waiting for consistency DB lock, trying to "
                         "take it. Current hash is: %s"), res.hash)
@@ -163,7 +163,94 @@ class HashHandler(object):
                  {'this': lock_id})
         return None
 
-    def read_for_update(self):
+    def _is_topo_sync_required(self, check_ts=True):
+        """Check consistencyhashes table if topo_sync is already in progress.
+
+        If topo_sync is in progress and timestamp of topo_sync is less than
+        TOPO_SYNC_EXPIRED_SECS, topo_sync is ACTIVE
+
+        Else topo_sync is INACTIVE
+
+        :return: (bool, timestamp_ms)
+                 boolean specifying if topo_sync is ACTIVE or not
+                 timestamp in milliseconds if available in database, else 0
+        """
+        res = self._get_current_record()
+        if res:
+            # record found, check other details
+            prev_ts = (self._get_lock_owner(res.hash)
+                       if 'LOCKED_BY' in res.hash
+                       else res.hash)
+
+            timestamp_expired, prev_ts = self._is_timestamp_expired(prev_ts)
+
+            if timestamp_expired:
+                LOG.debug("TOPO_SYNC: timestamp_expired "
+                          "_is_topo_sync_required: True")
+                return True, prev_ts
+            elif not check_ts:
+                # even if check_ts is False, we retrieve the prev_ts and return
+                LOG.debug("TOPO_SYNC: check_ts is False "
+                          "_is_topo_sync_required: True")
+                return True, prev_ts
+            else :
+                LOG.debug("TOPO_SYNC: timestamp_expired is False, "
+                          "check_ts is True _is_topo_sync_required: False")
+                return False, prev_ts
+        else:
+            # no record found, sync required
+            LOG.debug("TOPO_SYNC: no record found _is_topo_sync_required: "
+                      "True")
+            return True, '0'
+
+    def _is_timestamp_expired(self, prev_ts='0'):
+        """Check if prev_ts is older than TOPO_SYNC_EXPIRED_SECS
+
+        :param prev_ts: previous timestamp value from hash record or 0
+        :return: (bool, prev_ts)
+                 boolean True if prev_ts is < TOPO_SYNC_EXPIRED_SECS else False
+                 prev_ts value as normalized and used in the function
+        """
+        curr_ts_float = float(self.lock_ts)
+        try:
+            prev_ts_float = float(prev_ts)
+        except:
+            # prev_ts value was garbage, set it to 0
+            prev_ts = '0'
+            prev_ts_float = float(0)
+
+        time_diff = curr_ts_float - prev_ts_float
+        if time_diff < TOPO_SYNC_EXPIRED_SECS:
+            # diff is under the limit, NOT EXPIRED
+            LOG.debug("TOPO_SYNC: _is_timestamp_expired: Current timestamp is "
+                      "%(curr)s, previous timestamp is %(prev)s, "
+                      "diff between the two is %(diff)s. "
+                      "Diff < %(expired_secs)s, NOT EXPIRED.",
+                      {'curr': convert_ts_to_datetime(curr_ts_float),
+                       'prev': convert_ts_to_datetime(prev_ts_float),
+                       'diff': time_diff,
+                       'expired_secs': TOPO_SYNC_EXPIRED_SECS})
+            return False, prev_ts
+        else:
+            # diff is over the time range, EXPIRED
+            LOG.debug("TOPO_SYNC: _is_timestamp_expired: Current timestamp is "
+                      "%(curr)s, previous timestamp is %(prev)s, "
+                      "diff between the two is %(diff)s. "
+                      "Diff > %(expired_secs)s, EXPIRED.",
+                      {'curr': convert_ts_to_datetime(curr_ts_float),
+                       'prev': convert_ts_to_datetime(prev_ts_float),
+                       'diff': time_diff,
+                       'expired_secs': TOPO_SYNC_EXPIRED_SECS})
+            return True, prev_ts
+
+    def lock(self, check_ts=True):
+        """
+
+        :param check_ts:
+        :return: boolean sync_required
+                            True if all conditions met and lock acquired
+                            False if locking not required
+        """
         # An optimistic locking strategy with a timeout to avoid using a
         # consistency hash while another server is using it. This will
         # not return until a lock is acquired either normally or by stealing
@@ -171,10 +258,32 @@ class HashHandler(object):
         # MAX_LOCK_WAIT_TIME.
         lock_wait_start = None
         last_lock_owner = None
-        lock_id = self.random_lock_id
+        lock_ts = self.lock_ts
         retry_sleep_time = random.randint(MIN_LOCK_RETRY_SLEEP_TIME,
                                           MAX_LOCK_RETRY_SLEEP_TIME) / 1000.0
-        LOG.debug("This request's LockID is %s", lock_id)
+
+        # pre-check: is_topo_sync_active? if yes, don't contend for the lock
+        topo_sync_required, prev_ts = self._is_topo_sync_required(check_ts)
+
+        if not topo_sync_required:
+            return False
+
+        LOG.debug("TOPO_SYNC: current lock_ts timestamp is %s",
+                  convert_ts_to_datetime(lock_ts))
+        LOG.debug("TOPO_SYNC: previously executed at %s",
+                  convert_ts_to_datetime(prev_ts))
+        # if check_ts=True, execute only if diff > 1 hour
+        time_diff = float(lock_ts) - float(prev_ts)
+        if check_ts is True and time_diff < TOPO_SYNC_EXPIRED_SECS:
+            LOG.info(_LI("TOPO_SYNC: Last topo_sync executed at %(prev_ts)s, "
+                         "which is less than %(expired_sec)s seconds ago. "
+                         "Skipping current topo_sync."),
+                     {'prev_ts': convert_ts_to_datetime(prev_ts),
+                      'expired_sec': str(TOPO_SYNC_EXPIRED_SECS)})
+            return False
+
+        LOG.debug("This request's LockTS is %s",
+                  convert_ts_to_datetime(lock_ts))
         while True:
             res = self._get_current_record()
             if not res:
@@ -187,32 +296,50 @@ class HashHandler(object):
                     eventlet.sleep(retry_sleep_time)
                     continue
                 # The empty hash was successfully inserted with our lock
-                LOG.debug("LockID %s has grabbed the lock", lock_id)
-                return ''
+                LOG.debug("LockTS %s has grabbed the lock",
+                          convert_ts_to_datetime(lock_ts))
+                return True
 
             current_lock_owner = self._get_lock_owner(res.hash)
             if not current_lock_owner:
                 # no current lock. attempt to lock
-                new = self.lock_marker + res.hash
+                new = self.lock_marker
                 if not self._optimistic_update_hash_record(res, new):
                     # someone else beat us to it. restart process to wait
                     # for new lock ID to be removed
                     LOG.debug(
                         "Failed to acquire lock. Restarting lock wait. "
-                        "Previous hash: %(prev)s. Attempted update: %(new)s",
-                        {'prev': res.hash, 'new': new})
+                        "Previous lock_ts: %(prev)s. Attempted update: %(new)s",
+                        {'prev': convert_ts_to_datetime(res.hash), 'new': new})
                     eventlet.sleep(retry_sleep_time)
                     continue
                 # successfully got the lock
-                LOG.debug("LockID %s has grabbed the lock", lock_id)
-                return res.hash
+                LOG.debug("LockTS %s has grabbed the lock",
+                          convert_ts_to_datetime(lock_ts))
+                # now check timestamp if requested
+                timestamp_expired, prev_ts = self._is_timestamp_expired(
+                    res.hash)
+                if timestamp_expired or check_ts is False:
+                    LOG.debug("TOPO_SYNC: grabbed the lock and timestamp "
+                              "expired (or forced sync via check_ts False), "
+                              "sync required.")
+                    return True
+                elif check_ts is True and not timestamp_expired:
+                    # put the old timestamp back in the DB
+                    LOG.debug("TOPO_SYNC: check_ts is True and timestamp has "
+                              "not expired. Reverting the timstamp in DB and "
+                              "returning.")
+                    self.put_hash(res.hash)
+                    return False
 
-            if current_lock_owner == lock_id:
+            if current_lock_owner == lock_ts:
                 # no change needed, we already have the table lock due to
-                # previous read_for_update call.
+                # previous lock call.
                 # return hash with lock tag stripped off for use in a header
-                LOG.debug("LockID %s has grabbed the lock", lock_id)
-                return res.hash.replace(self.lock_marker, '')
+                LOG.debug("LockTS %s has grabbed the lock",
+                          convert_ts_to_datetime(lock_ts))
+                LOG.debug("TOPO_SYNC: prev_ts unavailable, sync required.")
+                return True
 
             cur_time = time.time()
             if current_lock_owner != last_lock_owner:
@@ -220,30 +347,52 @@ class HashHandler(object):
                 # wasn't to us. Reset the counter. Log if not
                 # first iteration.
                 if lock_wait_start:
-                    LOG.debug("LockID %(this)s - Lock owner changed from "
+                    LOG.debug("LockTS %(this)s - Lock owner changed from "
                               "%(old)s to %(new)s while waiting to acquire it",
-                              {'this': lock_id, 'old': last_lock_owner,
+                              {'this': convert_ts_to_datetime(lock_ts),
+                               'old': last_lock_owner,
                                'new': current_lock_owner})
+                    timestamp_expired, prev_ts = self._is_timestamp_expired(
+                        prev_ts=current_lock_owner)
+                    if check_ts is True and not timestamp_expired:
+                        LOG.debug("TOPO_SYNC: check_ts is True and another "
+                                  "thread with recent timestamp gained lock. "
+                                  "Giving up attempt to topo_sync.")
+                        return False
                 lock_wait_start = cur_time
                 last_lock_owner = current_lock_owner
 
             db_lock_hash = None
             time_waited = cur_time - lock_wait_start
-            if current_lock_owner.startswith(DBLOCK_PREFIX_TOPO):
-                # Extended timeout for TopoSync as it could take more time
-                if time_waited > MAX_LOCK_TOPOSYNC_WAIT_TIME:
-                    db_lock_hash = self._try_force_acquire_db_lock(res)
-            elif time_waited > MAX_LOCK_WAIT_TIME:
+            if time_waited > MAX_LOCK_WAIT_TIME:
                 db_lock_hash = self._try_force_acquire_db_lock(res)
 
             if db_lock_hash:
-                LOG.debug("LockID %s has grabbed the lock", lock_id)
-                return db_lock_hash
+                LOG.debug("LockTS %s has grabbed the lock",
+                          convert_ts_to_datetime(lock_ts))
+                LOG.debug("TOPO_SYNC: force grabbed the lock after waiting "
+                          "for MAX_LOCK_WAIT_TIME.")
+                return True
 
             eventlet.sleep(retry_sleep_time)
 
-    def clear_lock(self):
-        LOG.debug("Clearing hash record of LockID  %s", self.random_lock_id)
+    def put_hash(self, hash):
+        with self.session.begin(subtransactions=True):
+            res = (self.session.query(ConsistencyHash).
+                   filter_by(hash_id=self.hash_id).first())
+            if res:
+                res.hash = hash
+            else:
+                conhash = ConsistencyHash(hash_id=self.hash_id, hash=hash)
+                self.session.merge(conhash)
+        LOG.debug("Consistency hash for group %(hash_id)s updated "
+                  "to %(hash_ts)s by LockID %(this)s",
+                  {'hash_id': self.hash_id, 'hash': hash,
+                   'this': self.lock_ts})
+
+    def unlock(self):
+        LOG.debug("Unlocking and setting timestamp_ms to LockTS  %s",
+                  self.lock_ts)
         with self.session.begin(subtransactions=True):
             res = (self.session.query(ConsistencyHash).
                    filter_by(hash_id=self.hash_id).first())
@@ -257,76 +406,4 @@ class HashHandler(object):
                 LOG.warning(_LW("Another server already removed the lock. %s"),
                             res.hash)
                 return
-            res.hash = res.hash.replace(self.lock_marker, '')
-
-    def put_hash(self, hash):
-        hash = hash or ''
-        with self.session.begin(subtransactions=True):
-            res = (self.session.query(ConsistencyHash).
-                   filter_by(hash_id=self.hash_id).first())
-            if res:
-                res.hash = hash
-            else:
-                conhash = ConsistencyHash(hash_id=self.hash_id, hash=hash)
-                self.session.merge(conhash)
-        LOG.debug("Consistency hash for group %(hash_id)s updated "
-                  "to %(hash)s by LockID %(this)s",
-                  {'hash_id': self.hash_id, 'hash': hash,
-                   'this': self.random_lock_id})
-
-    def put_hash_if_owner(self, new_hash):
-        """Update the DB Hash if the current thread is the DB lock owner
-        @:return: True, if DB hash was successfully updated
-                  False, otherwise
-        """
-        new_hash = new_hash or ''
-        lock_marker = self.lock_marker + '%'
-        query = sa.update(ConsistencyHash.__table__).values(hash=new_hash)
-        query = query.where(ConsistencyHash.hash_id == self.hash_id)
-        query = query.where(ConsistencyHash.hash.like(lock_marker))
-
-        success = True
-        try:
-            with self._FACADE.get_engine().begin() as conn:
-                result = conn.execute(query)
-        except db_exc.DBDeadlock:
-            success = False
-
-        # We need to check update row count for successful update
-        if success and (result.rowcount != 0):
-            # DB Hash update was successful
-            LOG.debug("Consistency hash for group %(hash_id)s updated "
-                      "to %(hash)s by LockID %(this)s",
-                      {'hash_id': self.hash_id, 'hash': new_hash,
-                       'this': self.random_lock_id})
-            return True
-
-        LOG.debug("LockID %s is no longer DB lock owner. Consistency "
-                  "hash not updated", self.random_lock_id)
-        return False
-
-    def is_db_lock_owner(self):
-        """Check if the current thread is the DB lock owner
-        @:return True, if thread is the DB lock owner
-                 False, otherwise
-        """
-        res = self._get_current_record()
-        if not res:
-            return False
-
-        lock_owner = self._get_lock_owner(res.hash)
-        if not lock_owner:
-            return False
-
-        if lock_owner == self.random_lock_id:
-            return True
-        return False
-
-    def is_db_hash_empty(self):
-        """Check if DB hash record exists
-        :return: True, if there is no hash entry
-                 False, otherwise
-        """
-        if not self._get_current_record():
-            return True
-        return False
+            res.hash = res.hash.replace(self.lock_marker, self.lock_ts)
